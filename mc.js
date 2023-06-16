8e48835806a07ecec87a83541b555dafe6266734
Muhammet Caglar <1muhammetcaglar@gmail.com>
2023-06-17 00:46:21 +0300

    The code of print the result to the screen is added

--- mc.js
+++ mc.js
@@ -1,5 +1,6 @@
 const correctAnswers=['E','E','E','E'];
 const form=document.querySelector('.question-form');
+const result=document.querySelector('.result');
 form.addEventListener('submit',e=>{
     e.preventDefault();
     let score=0;
@@ -9,5 +10,7 @@ form.addEventListener('submit',e=>{
             score +=25;
         }
     });
-    console.log(score);
+    // console.log(score);
+    result.classList.remove('d-none');
+    result.querySelector('span').textContent=`${score}%`;
 });
