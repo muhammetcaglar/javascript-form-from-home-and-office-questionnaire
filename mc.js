1e881822ee8a5bfc3a44b0b025ee79515e5082c7
Muhammet Caglar <1muhammetcaglar@gmail.com>
2023-06-17 00:38:57 +0300

    Form function added

---
+++ mc.js
@@ -0,0 +1,13 @@
+const correctAnswers=['E','E','E','E'];
+const form=document.querySelector('.question-form');
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    let score=0;
+    const userAnswer=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+    userAnswer.forEach((answer,index)=>{
+        if(answer===correctAnswers[index]){
+            score +=25;
+        }
+    });
+    console.log(score);
+});
